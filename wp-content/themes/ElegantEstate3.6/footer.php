86730cdafbdce35d1820b8b681cc777df0c919c0
WebModerna@github.com <edgardogalletto@gmail.com>
2019-05-22 12:06:58 -0300

    Cambio de datos del footer

--- wp-content/themes/ElegantEstate3.6/footer.php
+++ wp-content/themes/ElegantEstate3.6/footer.php
@@ -2,9 +2,9 @@
 		</div> <!-- end #content-top-->
 		<div id="content-bottom">	</div>
 		<p id="copyright">
-			<span style="font-size:12px;">© Copyright <?php echo Date(Y); ?> Inmobiliaria de las Sierras.</span><br />
-			Lunes a viernes de 9hs a 17hs. Sábados de 9hs a 13hs.<br />
-			Salta esq. Ruta 15 | Villa Cura Brochero | Traslasierra | Córdoba | Argentina | <strong>Tel: 03544-15551212</strong><br />
+			<span style="font-size:12px;">© Copyright <?php echo Date(Y); ?> <?php bloginfo( "name" ); ?></span><br />
+			Lunes a sábados de 9hs a 13hs.<br />
+			Padre Ricotti esq. F. Altamirano (frente a Complejo Casaflor), Barrio Costa Azul, Villa Cura Brochero (5891) - Traslasierra - Provincia de Córdoba | <strong>Tel: +549 3544 551212 y 03544-471563</strong><br />
 			<strong>info@sierrasinmobiliaria.com.ar</strong> | <a href="http://www.sierrasinmobiliaria.com.ar" target="_blank">www.sierrasinmobiliaria.com.ar</a>
 		</p>
 		<p style="text-align:right !important; font-size:12px; ">Desarrollado por <a href="http://www.webmoderna.com.ar" target="_blank" style="text-align:right !important;font-size:12px !important;" >WebModerna</a>
